1d79077efbec2c7a2a0a768b8e73a9a87a04e856
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-09-21 17:44:50 -0500

    creacion esquema inicio, login y creacion rutas con react-router-dom

---
+++ tesla/src/Pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const home = () => {
+    return(
+        <React.Fragment>
+            <div>
+                esta es la pagina de inicio
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default home;
---
+++ tesla/src/Pages/login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const login = () => {
+    return(
+        <React.Fragment>
+            <div className="container">
+                <div className="registro">
+                    desea registrarse?
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default login;
