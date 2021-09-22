5f033f7f6cbf8cc4735673c80867f48776b8399f
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-09-22 07:33:52 -0500

    Creacion del login y sus estilos

--- tesla/src/Pages/login.js
+++ tesla/src/Pages/login.js
@@ -1,13 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+import './styles/login.css';
 
 const login = () => {
     return(
         <React.Fragment>
-            <div className="container">
-                <div className="registro">
-                    no tienes cuenta?, registrate ahora
-                    <Link to="/registrarse">Registrate aqui</Link>
+            <div className="main">
+                <div className="main-login">
+                    <div className="form">
+                        <Box
+                            component="form"
+                            sx={{
+                                '& > :not(style)': { m: 1, width: '100%' },
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        >   
+                            <div className="campos">
+                            <TextField id="filled-basic" label="Usuario" variant="filled" />
+                            </div>
+                            <div className="campos">
+                            <TextField id="filled-basic" label="contraseña" variant="filled" />
+                            </div>
+                        </Box>
+                        <div className="boton-login">
+                            <Button variant="contained" color="success">Iniciar sesion</Button>
+                        </div>
+                        <div className="registrarse">
+                            no tienes cuenta?, registrate ahora
+                            <Link to="/registrarse">Registrate aqui</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
