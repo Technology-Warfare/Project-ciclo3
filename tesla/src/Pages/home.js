94396031c10cd568153ce410b5200259197e9109
EillenC <ed_chb@hotmail.com>
2021-09-24 17:49:41 -0500

    crear css del footer, modificar css del home y añadir imagen para el home, además de crear unas secciones en el home

--- tesla/src/Pages/home.js
+++ tesla/src/Pages/home.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
+import fotoindex1 from "./img/fotoindex1.png";
+import './styles/home.css';
 
 const home = () => {
     return(
         <React.Fragment>
             <Navbar />
             <div className='mainContainer'>
-                esta es la pagina de inicio
+               <section className='section'>
+                   BUENAAAAS
+               </section>
+               <div className='contenedorImagen'><img src={fotoindex1} alt= "img1"/></div>
+               <section className='section2 contenedor'>
+                   <div className='caracteristicas'>Estamos diseñando y fabricando un ecosistema completo de energía y transporte que está totalmente integrado verticalmente.</div>
+                   <div className='caracteristicas2'>Buscamos el mayor beneficio ambiental posible desarrollando capacidades de fabricación avanzadas.</div>
+               </section>
             </div>
             <Footer/>
         </React.Fragment>
