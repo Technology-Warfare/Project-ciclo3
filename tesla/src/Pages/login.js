bac6edfef4b11a88fac8db4bd64dbfcedf0f869b
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-09-24 12:33:38 -0500

    acomodar boton para ingresar a adminvtas

--- tesla/src/Pages/login.js
+++ tesla/src/Pages/login.js
@@ -15,6 +15,7 @@ import './styles/login.css';
 import logo from './img/logo.png';
 
 const Login = () => {
+    
     const MyLink = props => <Link to="/adminvtas" {...props} />
 
     const [values, setValues] = React.useState({
