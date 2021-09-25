214068b3248a5394b31956bcc0f1134aac340857
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-09-25 13:07:02 -0500

    regresando a lo anterior

--- tesla/src/Pages/home.js
+++ tesla/src/Pages/home.js
@@ -4,15 +4,23 @@ import Footer from "../components/Footer"
 import BasicTabs from "../components/TabMenu";
 
 import './styles/home.css';
+import fotoindex1 from "../Pages/img/fotoindex1.png";
 
 const home = () => {
     return(
         <React.Fragment>
             <Navbar />
-            <BasicTabs />
             <div className='mainContainer'>    
-               
+                <section className='section'>
+                    BUENAAAAS
+                </section>
+                <div className='contenedorImagen'><img src={fotoindex1} alt= "img1"/></div>
+                <section className='section2 contenedor'>
+                    <div className='caracteristicas'>Estamos diseñando y fabricando un ecosistema completo de energía y transporte que está totalmente integrado verticalmente.</div>
+                    <div className='caracteristicas2'>Buscamos el mayor beneficio ambiental posible desarrollando capacidades de fabricación avanzadas.</div>
+                </section>
             </div>
+            <BasicTabs />
             <Footer/>
         </React.Fragment>
     );
--- tesla/src/components/ModelT.js
+++ tesla/src/components/ModelT.js
@@ -1,13 +1,11 @@
 import React from 'react'
 
-import '../Pages/styles/model.css';
 
 const ModelT = () => {
     return (
-        <section className='section2 contenedor'>
-           <div className='caracteristicas'>Estamos diseñando y fabricando un ecosistema completo de energía y transporte que está totalmente integrado verticalmente.</div>
-           <div className='caracteristicas2'>Buscamos el mayor beneficio ambiental posible desarrollando capacidades de fabricación avanzadas.</div>
-       </section>
+        <>
+            ModelT
+        </>
     )
 }
 
