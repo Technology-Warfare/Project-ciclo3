a62f16f23b3346ae4565890219064f33ec6d8018
Gitsjoe <jlopjen@gmail.com>
2021-10-02 14:52:34 -0500

    Botón Home añadido y algunos ajustes al sidebar

--- tesla/src/components/Sidebar.js
+++ tesla/src/components/Sidebar.js
@@ -6,27 +6,22 @@ const Sidebar = () => {
     return (
         <div className="sidebar">
             <ul className='mt-4'>
+                <li>
+                    <Link to="/" style={{ textDecoration: 'none' }}><div className='liu'><button type="button" class="btn btn-dark ButtonSidebar">Home</button></div></Link>
+                </li>
                 
-                <li className='mt-4'>
-                   
-                    <Link to="/dashboard/ventas" style={{ textDecoration: 'none' }}><div className='liu'> <button type="button" class="btn btn-dark ButtonSidebar">Ventas</button></div></Link>
-                    
+                <li>                   
+                    <Link to="/dashboard/ventas" style={{ textDecoration: 'none' }}><div className='liu'> <button type="button" class="btn btn-dark ButtonSidebar">Ventas</button></div></Link>  
                 </li>
                 <li>
-                    
                     <Link to="/dashboard/vehiculos" style={{ textDecoration: 'none' }}><div className='liu'><button type="button" class="btn btn-dark ButtonSidebar">Vehiculos
                     </button></div></Link>
-                    
                 </li>
                 <li>
-                    
                     <Link to="/dashboard/usuarios" style={{ textDecoration: 'none' }}><div className='liu'><button type="button" class="btn btn-dark ButtonSidebar">Usuarios </button></div></Link>
-                   
                 </li>
                 <li>
-                    
                     <Link to="/dashboard/" style={{ textDecoration: 'none' }}><div className='liu'><button type="button" class="btn btn-dark ButtonSidebar">Darchboard</button></div></Link>
-                    
                 </li>
             </ul>
         </div>
