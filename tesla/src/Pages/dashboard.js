b0dcc4562584bba4dff377799bfe3f4d0cee403e
Gitsjoe <jlopjen@gmail.com>
2021-10-02 15:48:29 -0500

    Carrousel agregado al dashboard

--- tesla/src/Pages/dashboard.js
+++ tesla/src/Pages/dashboard.js
@@ -4,44 +4,73 @@ import Footer from '../components/Footer';
 import Sidebar from '../components/Sidebar';
 import '../Pages/styles/dashboard.css';
 
+import fotoDashboard1 from "../Pages/img/ModelS/models2.jpg";
+import fotoDashboard2 from "../Pages/img/ModelX/modelx2.jpg";
+import fotoDashboard3 from "../Pages/img/ModelY/y.jpg";
+
 const dashboard = () => {
     return (
         <>
             <NavbarPrivado />
                 <div className="flex">
                     <Sidebar/>
-                    <div className="content justify-content-between d-flex  pb-5 displayFlexContent">
+                    <div className="content justify-content-between">
                         <div className="ColorBar">
-                        <div className='title card DisplayBlock Titulo'><h4 className="h4">Pepito Juarez</h4></div>
-                            <div className='card DisplayBlock'>
-                                <div className="FloatLeft">
-                                    <h6>Email:</h6> 
+                            <div className='title card DisplayBlock Titulo'><h4 className="h4">Pepito Juarez</h4></div>
+                                <div className='card DisplayBlock'>
+                                    <div className="FloatLeft">
+                                        <h6>Email:</h6> 
+                                    </div>
+                                    <div className="FloatRight">
+                                        pepitojuarez@gmail.com
+                                    </div>
                                 </div>
-                                <div className="FloatRight">
-                                    pepitojuarez@gmail.com
+                                <div className='card DisplayBlock'>
+                                    <div className="FloatLeft">
+                                        <h6>Edad:</h6>
+                                    </div>
+                                    <div className="FloatRight">
+                                        20 años
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='card DisplayBlock'>
-                                <div className="FloatLeft">
-                                    <h6>Edad:</h6>
+                                <div className='card DisplayBlock'>
+                                    <div className="FloatLeft">
+                                        <h6>Celular:</h6>
+                                    </div>
+                                    <div className="FloatRight">
+                                        3006587689
+                                    </div>                         
                                 </div>
-                                <div className="FloatRight">
-                                    20 años
-                                </div>
-                            </div>
-                            <div className='card DisplayBlock'>
-                                <div className="FloatLeft">
-                                    <h6>Celular:</h6>
-                                </div>
-                                <div className="FloatRight">
-                                    3006587689
-                                </div>                         
-                            </div>
-                        <div className="card DisplayBlock">
-                            <button className='rounded ButtonSize'>Editar Info</button>
+                            <div className="card DisplayBlock">
+                                <button className='rounded ButtonSize'>Editar Info</button>
                             </div>
                         </div>
+
+                    <div id="carouselExampleControls" class="carousel slide Carusel" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active paddingImg">
+                                <img src={fotoDashboard1} class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item paddingImg">
+                                <img src={fotoDashboard2} class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item paddingImg">
+                                <img src={fotoDashboard3} class="d-block w-100" alt="..." />
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
+
+
+                    </div>
+                    
                 </div>
             <Footer/>
         </>
