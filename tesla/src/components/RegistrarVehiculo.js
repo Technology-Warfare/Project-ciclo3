958af84f7e6d4c64d22ab559d8b9af630f85b6a9
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-10-08 15:46:33 -0500

    agregando cosas

--- tesla/src/components/RegistrarVehiculo.js
+++ tesla/src/components/RegistrarVehiculo.js
@@ -8,17 +8,18 @@ const RegistrarVehiculo = () => {
                     <div className="input-group flex-nowrap">
                         <span className="input-group-text">ID vehiculo</span>
                         <div className="form-floating">
-                            <input type="email" class="form-control" id="inIdVehiculo" />
-                            <label for="inIdVehiculo" class="form-label">TMM
+                            <input type="text" name="idVehiculo" class="form-control" id="inIdVehiculo" />
+                            <label for="inIdVehiculo" class="form-label">
+                                TMM
                             </label>
                         </div>
                         <label className="input-group-text" for="tipo">Tipo</label>
-                        <select className="form-select" id="tipo">
+                        <select name="tipo" className="form-select" id="tipo">
                             <option selected>Seleccione</option>
                             <option value="1">Electrico</option>
                         </select>
                         <label className="input-group-text" for="marca">Marca</label>
-                        <select className="form-select" id="marca">
+                        <select name="marca" className="form-select" id="marca">
                             <option selected>Seleccione</option>
                             <option value="1">Tesla</option>
                         </select>
@@ -29,21 +30,21 @@ const RegistrarVehiculo = () => {
                 <div className="form">
                     <div className="input-group">
                         <label className="input-group-text" for="modelo">Modelo</label>
-                        <select className="form-select" id="modelo">
+                        <select name="modelo" className="form-select" id="modelo">
                             <option selected>Seleccione</option>
                             <option value="1">Model S</option>
-                            <option value="1">Model 3</option>
-                            <option value="1">Model X</option>
-                            <option value="1">Model Y</option>
-                            <option value="1">Solar Roof</option>
-                            <option value="1">Solar Panels</option>
+                            <option value="2">Model 3</option>
+                            <option value="3">Model X</option>
+                            <option value="4">Model Y</option>
+                            <option value="5">Solar Roof</option>
+                            <option value="6">Solar Panels</option>
                         </select>
                     </div>
                 </div>
                 <div className="form">
                     <div className="input-group">
                         <label className="input-group-text" for="generacion">Generación</label>
-                        <select className="form-select" id="generacion">
+                        <select name="generacion" className="form-select" id="generacion">
                             <option selected>Seleccione</option>
                             <option value="1">1ra generación [2017 - 2021]</option>
                         </select>
@@ -54,7 +55,7 @@ const RegistrarVehiculo = () => {
                 <div className="form">
                     <div class="input-group mb-3">
                         <label className="input-group-text" for="serie">Serie</label>
-                        <select className="form-select" id="serie">
+                        <select name="serie" className="form-select" id="serie">
                             <option selected>Seleccione</option>
                             <option value="1">Sedan</option>
                         </select>
@@ -63,7 +64,7 @@ const RegistrarVehiculo = () => {
                 <div className="form">
                     <div className="input-group flex-nowrap">
                         <label className="input-group-text" for="modificacion">Modificación</label>
-                        <select className="form-select" id="modificacion">
+                        <select name="modificacion" className="form-select" id="modificacion">
                             <option selected>Seleccione</option>
                             <option value="1">190kw (258 cv) tracción, transmisión automática</option>
                             <option value="2">258kw (351 cv) tracción, transmisión automática</option>
@@ -77,7 +78,7 @@ const RegistrarVehiculo = () => {
                 <div className="form">
                     <div className="input-group flex-nowrap">
                         <label className="input-group-text" for="equipamento">Equipamento</label>
-                        <select className="form-select" id="equipamento">
+                        <select name="equipamiento" className="form-select" id="equipamento">
                             <option selected>Seleccione</option>
                             <option value="1">Long Range</option>
                             <option value="2">Performance</option>
@@ -88,11 +89,11 @@ const RegistrarVehiculo = () => {
             <div className="fForm">
                 <div className="form">
                     <label for="descripcion" className="form-label">Descripcion detallada</label>
-                    <textarea class="form-control" id="descripcion" rows="3"></textarea>
+                    <textarea name="descripcion" class="form-control" id="descripcion" rows="3"></textarea>
                 </div>
                 <div className="form">
                     <label for="observaciones" className="form-label">Observaciones generales</label>
-                    <textarea class="form-control" id="observaciones" rows="3"></textarea>
+                    <textarea name="observaciones" class="form-control" id="observaciones" rows="3"></textarea>
                 </div>
             </div>
         </div>
