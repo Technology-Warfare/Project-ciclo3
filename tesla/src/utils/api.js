11070bdff550d935583cb3da109a78da2213a27e
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-10-08 14:09:41 -0500

    pequeña correccion codigo

--- tesla/src/utils/api.js
+++ tesla/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-/*export const obtenerUsuarios = async (setUsuarios) => {
+export const obtenerUsuarios = async (setUsuarios) => {
     const options = { method: 'GET', url: 'http://localhost:5000/usuarios' };
     await axios
       .request(options)
@@ -10,18 +10,6 @@ import axios from 'axios';
       .catch(function (error) {
         console.error(error);
       });
-    };*/
-
-export const crearUsuario = async (setUsuarios) =>{
-    const options = { method:'POST', url: 'http://localhost:5000/usuarios/nuevo' };
-    await axios
-        .request(options)
-        .then(function (response){
-            setUsuarios(response.data);
-        })
-        .catch(function (error){
-            console.error(error);
-        });
-};
+    };
 
   
