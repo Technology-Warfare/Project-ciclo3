663b9dd2fdafad3c769ea257211817cbb5de7395
Gitsjoe <jlopjen@gmail.com>
2021-10-09 16:00:56 -0500

    Referencia de importacion incorrecta en archivo tabla.js, se comento para que dejara correr la aplicacion, (Corregir)

--- tesla/src/components/Table.js
+++ tesla/src/components/Table.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
 const DatosBack = () => {
